daa95330cfeefd77b61788c09520a3a7958e3b6c
Potatokujo <110933099+Potatokujo@users.noreply.github.com>
2023-10-23 17:28:43 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
 function tocarTeclaGrilo(){
-document.querySelector('#som_tecla_grilo').play();
+document.querySelector('.som_tecla_grilo').onclick = tocarTeclaGrilo;
+}
+
+function tocarAplausos(){
+document.querySelector('.tecla tecla_aplausos').onclick = tocarAplausos;
+}
+
+function tocarVaia(){
+document.querySelector('.tecla tecla_vaia').onclick = tocarVaia;
+}
+
+function tocarRisada(){
+document.querySelector('.tecla tecla_risada').onclick = tocarRisada;
+}
+
+function tocarErrado(){
+document.querySelector('.tecla tecla_errado').onclick = tocarErrado;
+}
+
+function tocarTambores(){
+document.querySelector('.som_tecla_tambores').onclick = tocarTambores;
+}
+
+function tocarTrombeta(){
+document.querySelector('.soom_tecla_trombeta').onclick = tocarTrombeta;
+}
+
+function tocarDinheiro(){
+document.querySelector('.som_tecla_dinheiro').onclick = tocarDinheiro;
+}
+function tocarVitoria(){
+document.querySelector('.som_tecla_vitoria').onclick = tocarVitoria;
 }
